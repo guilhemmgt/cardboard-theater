faebd25e33bb61b70558a0c6de6ff985746d2f8d
Flocondeglace <floriane.payen@gmail.com>
2026-01-31 23:45:44 +0100

    merge

--- ggj-2026/stages/_incidents.gd
+++ ggj-2026/stages/_incidents.gd
@@ -16,7 +16,7 @@ func _ready() -> void:
 	
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("test"):
-		all_incident.activate(5)
+		all_incident.activate(10)
 		# random_incident.activate(5)
 
 func update_ui_whisper(text:String):
