da14e69cf0648f51292834f94897248ee5c3917e
Flocondeglace <floriane.payen@gmail.com>
2026-01-31 17:55:27 +0100

    debug: timer solved

--- ggj-2026/failedScript.gd
+++ ggj-2026/failedScript.gd
@@ -12,7 +12,7 @@ func _ready() -> void:
 		animation_player.stop()
 	)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 func _on_repair_incident_activated() -> void:
