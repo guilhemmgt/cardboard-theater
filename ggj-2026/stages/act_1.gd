055786c6d56ebf32aad3af4df39948333d026332
Flocondeglace <floriane.payen@gmail.com>
2026-02-01 17:06:35 +0100

    chaipas

--- ggj-2026/stages/act_1.gd
+++ ggj-2026/stages/act_1.gd
@@ -7,4 +7,6 @@ func _on_act_1_animation_finished(anim_name: StringName) -> void:
 		print("WIIIIIIN")
 		var win :Win= WIN.instantiate()
 		add_child(win)
+		win.global_position = Vector3(0,3.394,0)
+		win.global_rotation_degrees = Vector3(0,-180,0)
 		win.play()
