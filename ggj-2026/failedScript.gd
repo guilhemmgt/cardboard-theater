2471c13e5dd3179a8f9a2865836148023869d0c2
Telessia <dunty.telessia@proton.me>
2026-02-01 14:34:53 +0100

    Revert "Merge branch 'main' of github.com:guilhemmgt/ggj2026"
    This reverts commit d8f07db2b80162dd25db2eb27d691176af033936, reversing
    changes made to da3d19a62164be7157b18284b1372b7c282e818a.

--- ggj-2026/failedScript.gd
+++ ggj-2026/failedScript.gd
@@ -12,18 +12,20 @@ func _ready() -> void:
 			repair_incident = child as RepairIncident
 			break
 
-	# events setup
-	print("[repair] Ready on: ", name, " - RepairIncident: ", repair_incident)
-	repair_incident.activated.connect(_on_incident_activated)
-	repair_incident.failed.connect(_on_incident_failed)
-	repair_incident.resolved.connect(_on_incident_resolved)
+	print("[failedScript] Ready on: ", name, " - RepairIncident: ", repair_incident)
+	repair_incident.activated.connect(_on_repair_incident_activated)
+	repair_incident.resolved.connect(func():
+		print("[failedScript] Resolved on: ", name)
+		animation_player.stop()
+	)
+	repair_incident.failed.connect(_on_repair_failed)
 
-func _on_incident_activated(_blocking: bool) -> void:
-	print("[repair] Activated on: ", name, " - Playing tremblement on AnimationPlayer: ", animation_player)
+
+func _on_repair_incident_activated(_blocking: bool) -> void:
+	print("[failedScript] Activated on: ", name, " - Playing tremblement on AnimationPlayer: ", animation_player)
 	animation_player.play("tremblement")
 
-func _on_incident_failed() -> void:
-	print("[intrusion] Failed on: ", name)
+func _on_repair_failed():
 	var angle : int = 5
 	var time_oscillation : float = 0.08
 	animation_player.stop()
@@ -35,7 +37,3 @@ func _on_incident_failed() -> void:
 		tween.chain().tween_property(self, "rotation_degrees", self.rotation_degrees + Vector3(0,0,angle), time_oscillation)
 		tween.chain().tween_property(self, "rotation_degrees", self.rotation_degrees + Vector3(0,0,-angle), time_oscillation)
 	tween.play()
-
-func _on_incident_resolved() -> void:
-	print("[intrusion] Resolved on: ", name)
-	animation_player.stop()
