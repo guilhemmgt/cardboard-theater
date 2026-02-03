19e3214119476c1a8989c95edb3aa422dd3262c5
Guilhem Mangenot <guilhem.mangenot@gmail.com>
2026-02-03 23:58:12 +0100

    feat: mover

--- ggj-2026/entities/theater/theater.gd
+++ ggj-2026/entities/theater/theater.gd
@@ -1,1 +1,20 @@
-extends Node3D
+extends Node
+class_name Theater
+
+# 3 plans (depth)
+# 6 positions (width)
+# 3x6=18 markers
+
+@export var plans: Node
+
+var _markers: Array[Array]
+
+func _ready() -> void:
+	for plan in plans.get_children():
+		var plan_positions: Array = []
+		for pos: Marker3D in plan.get_children():
+			plan_positions.append(pos.global_position)
+		_markers.append(plan_positions)
+
+func get_marker(plan: int, pos: int) -> Vector3:
+	return _markers[plan][pos]
