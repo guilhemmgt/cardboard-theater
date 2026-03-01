e2b0fc7d558b26e15dcc035dff940d9ff0112c64
Guilhem Mangenot <guilhem.mangenot@gmail.com>
2026-03-01 16:06:27 +0100

    chore: sorting more folders

---
+++ ggj-2026/globals/signal_bus.gd
@@ -0,0 +1,32 @@
+extends Node
+
+# Signals are either :
+# - requests : the emitter requests the execution of an action
+#                named with an imperative verb : "do_something"
+# - reports  : the emitter reports the execution of an action
+#                named with a past participle : "something_done"
+
+@warning_ignore_start("unused_signal")
+
+# UI signals
+signal toggle_music ## Mute/unmute the music
+signal play_sfx(stream: AudioStream) ## Play a SFX
+
+# Theater signals
+signal toggle_curtains ## Open/close the curtains
+signal curtains_opening_started ## The curtains are closed but are about to open
+signal curtains_opened ## The curtains are fully opened
+signal curtains_closed ## The curtains are fully closed
+signal init_curtains(open : bool) ## Set curtains position instantly (open/close)
+
+signal ask_to_move(node: Node3D, duration: float, plan: int, pos: int)
+
+# Tomato
+signal tomato_picked ## A tomato was picked, the player is now aiming
+signal tomato_released ## A tomato was either put down in the basket, or thrown at something
+signal tomato_hit(body : Node3D, point : Vector3) ## A tomato has hit a body
+
+# Act signals
+signal act_start
+
+@warning_ignore_restore("unused_signal")
